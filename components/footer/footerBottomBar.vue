eed543ff58b3072f8314c4a0a0ad40db20de60c6
Coranet <coranetworking@gmail.com>
2024-02-20 13:56:47 +0100

    Mise à jour des fichiers du footer

--- components/footer/footerBottomBar.vue
+++ components/footer/footerBottomBar.vue
@@ -1,5 +1,26 @@
 <template>
-
-    
+    <nav class="flex justifyBetween">
+        <ul>
+           <li>Réalisé par Posoroko 2024</li>
+           <li>Assisté par Coranet 2024</li>
+        </ul>
+    </nav>
 </template>
 
+<style scoped>
+nav {
+    padding: 5px;
+    background-color: #e1e1e1;
+    
+}
+
+ul {
+    display: flex;
+    justify-content: center;
+    gap: 20px;
+}
+
+li {
+    color: #0000008e;
+}
+</style>
--- components/footer/footerMain.vue
+++ components/footer/footerMain.vue
@@ -1,4 +1,14 @@
-<template>
 
-    
+
+<template>
+    <footer>
+        <FooterMiddleLinks />
+        <FooterBottomBar />
+    </footer>
 </template>
+
+<style scoped>
+footer {
+    height: 80px;
+}
+</style>
