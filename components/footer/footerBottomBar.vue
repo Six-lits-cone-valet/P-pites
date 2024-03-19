1b5dff494772856750df9e76d6f8920cd06512d8
Eric <eric@posoroko.com>
2024-03-19 13:04:32 +0100

    footer text adjustments

--- components/footer/footerBottomBar.vue
+++ components/footer/footerBottomBar.vue
@@ -1,8 +1,10 @@
 <template>
     <nav class="flex justifyBetween">
-        <ul>
-           <li>Réalisé par Posoroko 2024</li>
-           <li>Assisté par Coranet 2024</li>
+        <ul class="w100 flex justifyCenter gap20">
+            <li class="text-12">© 2024 Pépites</li>
+           <li class="text-12">Réalisé par Posoroko</li>
+
+           <li class="text-12">Assisté par Coranet</li>
         </ul>
     </nav>
 </template>
@@ -10,17 +12,8 @@
 <style scoped>
 nav {
     padding: 5px;
-    /*background-color: #e1e1e1;*/
-    
 }
-
-ul {
-    display: flex;
-    justify-content: center;
-    gap: 20px;
-}
-
 li {
-    /*color: #0000008e;*/
+    color: var(--gray-dimmed);
 }
 </style>
