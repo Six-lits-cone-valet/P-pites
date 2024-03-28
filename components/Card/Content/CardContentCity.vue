410bc3ac25782b3ed50c0997dc64a78cc2f24c74
Coranet <coranetworking@gmail.com>
2024-03-28 15:51:14 +0100

    mise à jour CityCards

--- components/Card/Content/CardContentCity.vue
+++ components/Card/Content/CardContentCity.vue
@@ -11,33 +11,42 @@ const props = defineProps({
 </script>
 
 <template>
-    <div class="cardContentBox portrait">
-    <h1 class="cardTitle boxLevel1">
+<div class="cardContentBox portrait">
+    <h1 class="box title cardTitle">
         {{ item.name }},
         <span class="grow" v-if="item.country">{{ item.country }}</span>
     </h1>     
 
-    <p class="box business flex alignCenter gap20 overflowHidden" v-if="item.business.length"> 
-        <svg viewBox="0 -960 960 960" class="cardIcon shrink0">
-            <path :d="icons.storeFront.path" />
-        </svg>
+    <div class="portraitContent">
+        <ul class=" icons grow flex column">
+            <div class="box flex alignCenter gap10">
 
-        <span class="grow">{{ item.business.length }} entreprises</span> 
-    </p>
+                <li class="box business flex alignCenter gap20 overflowHidden" v-if="item.Population"> 
+                    <svg viewBox="0 -960 960 960" class="cardIcon shrink0">
+                        <path :d="icons.groups.path" />
+                    </svg>
 
-    <p class="box business flex alignCenter gap20 overflowHidden" v-if="item.Population"> 
-        <svg viewBox="0 -960 960 960" class="cardIcon shrink0">
-            <path :d="icons.groups.path" />
-        </svg>
+                    <span class="grow">{{ item.Population }}</span> 
+                </li>
+            </div>
 
-        <span class="grow">{{ item.Population }}</span> 
-    </p>
+            <li class="box business ellipsis flex alignCenter gap20 overflowHidden" v-if="item.business"> 
+                <svg viewBox="0 -960 960 960" class="cardIcon shrink0">
+                    <path :d="icons.storeFront.path" />
+                </svg>
 
-    <p v-if="landscape"
-        class="description">
-        {{ item.description.slice(0,400) }}
-        <span class="italic text-14"> ... lire la suite</span>
-    </p>
+                <span class="infoText grow">{{ item.business.length }}</span> 
+            </li>
+        </ul>            
+    </div>
+
+    <div class="landscapeContent">
+        <p v-if="landscape"
+            class="box description">
+            {{ item.description.slice(0,400) }}
+            <span class="italic text-14"> ... lire la suite</span>
+        </p>
+    </div>
 </div>
     
   
