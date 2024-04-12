e81f6c41bea17d7a284f3906d492c8ff1bbf4d4c
Eric <eric@posoroko.com>
2024-04-12 09:41:49 +0200

    header

---
+++ components/section/Welcome.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="frame relative">
+        <img src="/images/food-trucks.jpg" alt="">
+
+        <h1>Pépites</h1>
+    </div>
+    
+</template>
+
+<style scoped>
+h1 {
+    width: 100%;
+    font-size: 10vw;
+    line-height: 1em;
+    text-align: center;
+    position: absolute;
+    top: 0px;
+    left: 0;
+}
+</style>
